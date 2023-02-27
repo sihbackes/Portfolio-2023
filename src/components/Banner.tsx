1a463bd5725ad4060a94c44c7476a45281ecd711
Simone Backes <88430766+sihbackes@users.noreply.github.com>
2023-02-27 17:34:52 +0000

    resume

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,6 +8,7 @@ return(
       <h3 className="font-roboto pb-6">Hi there,</h3>
       <h1 className="text-6xl font-nunito font-extrabold pb-6">I'm Simone Backes</h1>
       <h3 className="font-roboto text-lg">I'm a Front-end Developer. Welcome to my portfolio.</h3>
+
     </div>
   </section>
 )
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -37,16 +37,16 @@ return(
       </div>
       {isOpen && (
         <div className="absolute top-0 right-0 bg-white w-48 py-2 mt-12 rounded-lg shadow-xl dark:bg-zinc-800 dark:text-gray-200 z-10">
-          <HashLink smooth to="/#about" className="block px-4 py-2 text-zinc-800 hover:text-white hover:bg-zinc-500  dark:text-gray-300">
+          <HashLink smooth to="/#about" className="block px-4 py-2 text-zinc-800 hover:text-white hover:bg-zinc-500  dark:text-gray-300" onClick={() =>{setIsOpen(false)}}>
             About me
           </HashLink >
-          <HashLink smooth to="/#projects" className="block px-4 py-2 text-zinc-800  hover:text-white hover:bg-zinc-500 dark:text-gray-300">
+          <HashLink smooth to="/#projects" className="block px-4 py-2 text-zinc-800  hover:text-white hover:bg-zinc-500 dark:text-gray-300" onClick={() =>{setIsOpen(false)}}>
             Projects
           </HashLink >
-          <HashLink smooth to="/#contact" className="block px-4 py-2 text-zinc-800  hover:text-white hover:bg-zinc-500 dark:text-gray-300">
+          <HashLink smooth to="/#contact" className="block px-4 py-2 text-zinc-800  hover:text-white hover:bg-zinc-500 dark:text-gray-300" onClick={() =>{setIsOpen(false)}}>
             Contact
           </HashLink>
-          <div className="block px-4 py-2 text-zinc-800  hover:text-white hover:bg-zinc-500 dark:text-gray-300">
+          <div className="block px-4 py-2 text-zinc-800  hover:text-white hover:bg-zinc-500 dark:text-gray-300" onClick={() =>{setIsOpen(false)}}>
             <a href="/docs/SimoneBackes-cv.pdf" target="_blank">Resume</a>
           </div>
         </div>
@@ -67,7 +67,7 @@ return(
              <span className="border-r border-zinc-800 dark:border-gray-300"></span>
             </li>
             <li className="px-5">
-              <a href="/docs/SimoneBackes-cv.pdf" target="_blank">Resume</a>
+              <a href="/docs/SimoneBackes-Resume.pdf" target="_blank">Resume</a>
             </li>
           </ul>
         </nav>
