f8b87e0ca226fc4b3d5088f4d2223f50f0bce00c
Simone Backes <simonebackesv@gmail.com>
2024-08-19 10:28:08 +0100

    update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,7 +7,7 @@ return(
     <div className="mx-auto py-10 md:py-32">
       <h3 className="font-roboto pb-6">Hi there,</h3>
       <h1 className="text-6xl font-nunito font-extrabold pb-6">I'm Simone Backes</h1>
-      <h3 className="font-roboto text-lg">I'm a Frontend Developer. Welcome to my portfolio.</h3>
+      <h3 className="font-roboto text-lg">I'm a Web Developer. Welcome to my portfolio.</h3>
 
     </div>
   </section>
